0f62836c1dcc7491ad3a6e4c5dc6b9270f813625
marcosc11195 <eteaac@gmail.com>
2025-12-12 17:39:56 +0100

    gitignore succes

--- includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Configuración de conexión
-define("DB_HOST", "localhost");
-define("DB_USER", "root");     // cambia por tu usuario real
-define("DB_PASS", "");    // cambia por tu contraseña real
-define("DB_NAME", "formacomempleo");
-?>
