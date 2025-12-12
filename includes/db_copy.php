19ae6e9be2c76544638d6370510b64946799d8dd
marcosc11195 <eteaac@gmail.com>
2025-12-12 17:31:34 +0100

    gitignore

---
+++ includes/db_copy.php
@@ -0,0 +1,7 @@
+<?php
+// Configuración de conexión
+define("DB_HOST", "");
+define("DB_USER", "");     // cambia por tu usuario real
+define("DB_PASS", "");    // cambia por tu contraseña real
+define("DB_NAME", "");       // nombre de la BD
+?>
